8e1ebbd5695a7a8b63f16e6567c43d957bb04d5f
cmrsantos <cmrsantos@ua.pt>
2022-10-10 14:55:32 +0100

    refazer

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import argparse
 def main():
     #-h
     parser=argparse.ArgumentParser(
-        description='''Definition of test mode ''')
+        description='''Definition of test mode ''') 
     parser.add_argument('-utm', '--use_time_mode', type=int, required=False,
-        help=" Max number of secs for time mode or maximum number of inputs for number of inputs mode.")
+        help=" Max number of seconds.")
     parser.add_argument('-mv', '--max_value', type=int, required=False, 
-        help=" Max number of seconds for time mode or maximum number of inputs for number of inputs mode.")
+        help=" maximum number of inputs.")
     args=parser.parse_args()
 
 
     # para saber como se chamam
+
     # print(args)
-    # print(args.use_time_mode)
-    # print(args.max_value)
+    print(args.use_time_mode)
+    print(args.max_value)
+
     
 if __name__ == '__main__':
     main()
