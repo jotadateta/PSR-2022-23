d4ccd9c64693eb2f9994f32f1158c3877e135418
cmrsantos <cmrsantos@ua.pt>
2022-10-10 16:45:16 +0100

    10 de out 16:45 nov atualização

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from colorama import Fore, Back, Style
 
 def int_pos(s: str)  -> int:
     # torna o tipo de numero inteiro positivo
@@ -17,18 +18,31 @@ def main():
     #-h
     parser=argparse.ArgumentParser(
         description='''Definition of test mode ''') 
-    parser.add_argument('-utm', '--use_time_mode', type=int_pos, 
+    parser.add_argument('-utm', '--use_time_mode', action='store_true', 
         help=" Max number of seconds.")
     parser.add_argument('-mv', '--max_value', type=int_pos, required=False, 
-        help=" Maximum number of inputs.")
+        help=" Imput number.")
     args=parser.parse_args()
 
 
-    # para saber como se chamam
+    # para saber como se chamam as variaveis de entrada
+        # print(args)
+        # print(args.use_time_mode) #string o valor
+        # print(args.max_value) #o valor sai em string
+    print(vars(args))
+    print("Typing Test, group 5, October 2022")
 
-    # print(args)
-    print(args.use_time_mode) #string o valor
-    print(args.max_value) #o valor sai em string
+    # detetar o modo se é por tempo ou tentativas.
+    if args.use_time_mode==True:
+        if str(args.max_value)=='None':
+            print('Insert a mv MAX_NUMBER')
+        else:
+            print('Test runnin up to '+ str(args.max_value) + ' seconds')
+    else:
+        print('Test runnin up to '+ str(args.max_value) + ' inputs')
+
+    
+
 
 
 if __name__ == '__main__':
