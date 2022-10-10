1eff414531694e47300ce6b7cff1d3c6a8064649
cmrsantos <cmrsantos@ua.pt>
2022-10-10 14:40:51 +0100

    ficheiro que já estava iniciado

--- main.py
+++ main.py
@@ -2,23 +2,21 @@
 import argparse
 
 
-parser=argparse.ArgumentParser(
-    description='''Definition of test mode ''')
-parser.add_argument('-utm', '--use_time_mode', action='store_true',
-    help=" Max number of secs for time mode or maximum number of inputs for number of inputs mode.")
-parser.add_argument('-mv MAX_VALUE', '--max_value MAX_VALUE', action='store_true', 
-    help=" Max number of seconds for time mode or maximum number of inputs for number of inputs mode.")
-args=parser.parse_args()
-
-
-
-print(args)
-print(args.max_value)
-# args.use_time_mode
-
-
 def main():
-   ""
+    #-h
+    parser=argparse.ArgumentParser(
+        description='''Definition of test mode ''')
+    parser.add_argument('-utm', '--use_time_mode', type=int, required=False,
+        help=" Max number of secs for time mode or maximum number of inputs for number of inputs mode.")
+    parser.add_argument('-mv', '--max_value', type=int, required=False, 
+        help=" Max number of seconds for time mode or maximum number of inputs for number of inputs mode.")
+    args=parser.parse_args()
+
+
+    # para saber como se chamam
+    # print(args)
+    # print(args.use_time_mode)
+    # print(args.max_value)
     
 if __name__ == '__main__':
     main()
