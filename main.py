7310b0093e979c2376f5633e74c4fccfd92f33fd
lclem0 <jclemente@ua.pt>
2022-10-10 21:49:04 +0100

    fiz um bocado do main loop

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import argparse
+from collections import namedtuple
+import string
+import random
+import time
+from urllib import request
 from colorama import Fore, Back, Style
 from readchar import readkey, key
 
@@ -15,6 +20,10 @@ def int_pos(s: str)  -> int:
     return v
 
 
+def obter_args(args):
+    return args['use_time_mode'], args['max_value']
+
+
 def main():
     #-h
     parser=argparse.ArgumentParser(
@@ -26,6 +35,21 @@ def main():
     args=parser.parse_args()
 
 
+    use_time_mode, max_value = obter_args(args)
+    
+    data= {
+        'accuracy': 0.0,
+        'inputs': [],
+        'number_of_hits': 0,
+        'number_of_types': 0,
+        'test_duration': 0.0,
+        'test_end': '',
+        'test_start': '',
+        'type_average_duration': 0.0,
+        'type_hit_average_duration': 0.0,
+        'type_miss_average_duration': 0.0
+    }
+   
     # para saber como se chamam as variaveis de entrada
         # print(args)
         # print(args.use_time_mode) #string o valor
@@ -43,9 +67,48 @@ def main():
         print('Test runnin up to '+ str(args.max_value) + ' inputs')
 
     #basta clicar numa tecla para continuar o programa
-    print('Press any key to start the test')
+    print('Press any key to start the test')  
     k = readkey()
 
+    Input = namedtuple('Input',([requested,received,duration]))
+    
+
+    inicial_time = time()
+    
+    # while loop
+    while True:
+
+        #start game time
+        count_time = time()
+
+        #gera letra aleatória
+        requested =  random.choice(string.ascii_letters)  
+        print('Type letter'+ Fore.YELLOW + requested + Style.RESET_ALL)
+
+        #read letter input
+        received = readkey()
+        if requested == ' ':
+            break
+        elif requested == received:
+            print('You typed ' + Fore.GREEN + received + Style.RESET_ALL)
+        else:
+            print('You typed ' + Fore.RED + received + Style.RESET_ALL)
+
+        
+
+
+
+            
+
+
+
+
+
+
+
+
+
+    
 
     
 if __name__ == '__main__':
